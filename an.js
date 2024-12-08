32694558c35f0785a97e06231b735c5da213f2e4
Syed Basit Ali <Basitali.vision@gmail.com>
2024-12-09 01:00:03 +0500

    Update an.js

--- an.js
+++ an.js
@@ -1,21 +1,31 @@
+let num1 = prompt("Enter the first number:");
+let num2 = prompt("Enter the second number:")
 
-let num1 = parseFloat(prompt("Enter the first number:"));
-let num2 = parseFloat(prompt("Enter the second number:"));
-let sum = num1 + num2;
-alert("The sum of ${num1} and ${num2} is ${sum}.");
+num1 = parseFloat(num1);
+num2 = parseFloat(num2)
+let sum = num1 + num2
 
-
-
+alert(`The sum of ${num1} and ${num2} is ${sum}`)
 
 let userName = prompt("Enter your name:");
-let purchaseAmount = parseFloat(prompt("Enter the total purchase amount:"));
-let discount = 10; 
-let discountAmount = (purchaseAmount * discount) / 100;
-let finalAmount = purchaseAmount - discountAmount;
-alert("Hello ${userName}, your final amount after a 10% discount is ${finalAmount.toFixed(2)}.");
 
+let purchaseAmount = prompt("Enter the total purchase amount:")
 
+purchaseAmount = parseFloat(purchaseAmount)
 
+let discount = 10
 
-let favoriteAnimal = prompt("What is your favorite animal?");
-alert("Wow, ${favoriteAnimal}s are amazing creatures!");
+let discountAmount = (purchaseAmount * discount) / 100
+
+let finalAmount = purchaseAmount - discountAmount
+
+alert(`${userName}, after a 10% discount, the final amount to pay is $${finalAmount.toFixed(2)}`)
+
+let favoriteAnimal = prompt("What is your favorite animal?")
+
+if (favoriteAnimal.toLowerCase() === "cat") {
+  
+    alert("Wow, cats are amazing creatures!");
+} else {
+    alert(`Wow, ${favoriteAnimal}s are awesome too!`);
+}
